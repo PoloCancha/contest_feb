b4e70cf0146ea84a23b4582115962de26a782217
PoloCancha <pabscampana@gmail.com>
2026-02-17 18:49:11 +0100

    Session 9

--- list_methods.py
+++ list_methods.py
@@ -3,16 +3,20 @@ from asyncio import print_call_graph
 a = [1, 2, 3,]
 a.append(27) # adds 1 item to the end
 print(a)
-b = ["Bob"] * 4
+b = ["Bob"] * 14
 a.extend(b) # adds one list at the end, same as a + b
 print(a)
 
-# pip removes by position
+# pop removes by position
 x = a.pop(3)
 print(x)
 print(a)
 
-a.remove("Bob")
-print(a)
 a.remove(3)
+print(a)
+while True:
+    try:
+        a.remove("Bob")
+    except ValueError:
+        break
 print(a)
